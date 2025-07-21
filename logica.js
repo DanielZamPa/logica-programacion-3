c54fbab9277884d7acf632e35bf0750b8692d7bc
Daniel Zamora Paez <[email>
2025-07-21 10:50:09 -0500

    Inicializacion del repositorio

---
+++ logica.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync");
+
+let num = []
+
+num.push(prompt("Ingrese un numero: "));
+num.push(prompt("Ingrese otro numero: "));
+num.push(prompt("Ingrese un tercer numero: "));
+
+let save = 0;
+
+for (let i=0; num.length; i++){
+    
+}
+
